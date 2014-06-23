a5d0b25084101ccd915b2afe9bd9c54cca5e26a6
Richard Forrest <richard.a.forrest@gmail.com>
2014-06-23 15:25:27 +0100

    Initial commit

---
+++ src/SuperMember.Sample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SuperMember.Sample.Startup))]
+namespace SuperMember.Sample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
